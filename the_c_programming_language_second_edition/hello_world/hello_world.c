81f511346d5c564298c3657a371a933209eac999
RobertBehring <rsbehring@gmail.com>
2022-12-10 20:45:09 -0800

    update to hello world

--- the_c_programming_language_second_edition/hello_world/hello_world.c
+++ the_c_programming_language_second_edition/hello_world/hello_world.c
@@ -3,8 +3,5 @@
 int main(void) {
     int user_num;
     printf("Hello World!\n");
-    printf("Enter a number: ");
-    scanf("%d", &user_num);
-    printf("You entered %d\n", user_num);
     return 0;
 }
