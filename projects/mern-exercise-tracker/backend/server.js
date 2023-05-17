75ba06b7a1b659410e3b76cf8c3fcee7dd3f9625
RobertBehring <rsbehring@gmail.com>
2023-05-17 14:29:58 -0400

    add: CRUD operations for users, exercises

--- projects/mern-exercise-tracker/backend/server.js
+++ projects/mern-exercise-tracker/backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose');
-const {MongoClient, ServerApiVersion} = require("mongodb");
+const mongoose = require('mongoose');
+// const {MongoClient, ServerApiVersion} = require("mongodb");
 
 require('dotenv').config();
 
@@ -12,31 +12,37 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true});
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors:true,
-    }
-});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ping:1});
-        console.log("MongoDB database connection established successfully");
-    } finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors:true,
+//     }
+// });
+//
+// async function run() {
+//     try {
+//         await client.connect();
+//         await client.db("admin").command({ping:1});
+//         console.log("MongoDB database connection established successfully");
+//     } finally {
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
-app.user('/users', usersRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
